97abe69848c92b811b7168312d9870429c4dc83e
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-21 02:32:37 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,7 +56,7 @@ end
 def self.find_by(value)
   #binding.pry
   column_names.each do |column|
-    sql = "SELECT * FROM #{self.table_name} WHERE #{send(column)} = #{value.flatten[1]}"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{column} = #{value.flatten[1]}"
     binding.pry
     DB[:conn].execute(sql)
   end
