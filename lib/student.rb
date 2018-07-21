b7253d39d81010dd4dba855bd9d67dd06e536c14
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-21 02:12:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-require_relative './lib/interactive_record.rb'
+require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
