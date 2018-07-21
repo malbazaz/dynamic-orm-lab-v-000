5c862f5fc3f57de91eca8eaa285c4ff7f1039f8b
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-21 02:22:04 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,8 +53,10 @@ def self.find_by_name(name)
   DB[:conn].execute(sql)
 end
 
-def self.find_by()
-
+def self.find_by(value)
+  column_names.each do |column|
+  sql = "SELECT * FROM #{self.table_name} WHERE #{column} = #{value}"
+  DB[:conn].execute(sql)
 end
 
 end
