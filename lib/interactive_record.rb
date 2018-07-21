c84af1d4f010fbe14ceb541653c4efe2d63a3026
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-21 02:11:59 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'pry'
 class InteractiveRecord
-  
+
   def self.table_name
     self.to_s.downcase.pluralize
   end
